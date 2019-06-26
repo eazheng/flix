d564e5104257647c4d22e991c18e9d4574f50d72
Emily Zheng <eazheng@fb.com>
2019-06-26 15:24:32 -0700

    added table view setup and custom cells to display title and synopsis of movie

---
+++ flix/Views/MovieCell.h
@@ -0,0 +1,20 @@
+//
+//  MovieCell.h
+//  flix
+//
+//  Created by eazheng on 6/26/19.
+//  Copyright © 2019 eazheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+
+@end
+
+NS_ASSUME_NONNULL_END
